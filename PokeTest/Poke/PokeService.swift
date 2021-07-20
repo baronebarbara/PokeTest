cdf2a763d575d4e049f6597a6386e6f02b472ae7
Barbara Barone <barbara.barone@picpay.com>
2021-07-20 19:53:27 -0300

    Implementando service

--- PokeTest/Poke/PokeService.swift
+++ PokeTest/Poke/PokeService.swift
@@ -15,7 +15,21 @@ final class PokeService {
 // MARK: - PokeServicing
 extension PokeService: PokeServicing {
     func fetch(completion: @escaping (Result<Pokemon, APIError>) -> Void) {
-        
+        let session = URLSession.shared
+        guard let url = URL(string: "https://pokeapi.co/api/v2/pokemon/charmander") else { return }
+        let task = session.dataTask(with: url) { data, response, error in
+            guard let data = data else { return }
+            let decode = JSONDecoder()
+            decode.keyDecodingStrategy = .convertFromSnakeCase
+            DispatchQueue.main.async {
+                do {
+                    let decoded = try decode.decode(Pokemon.self, from: data)
+                    completion(.success(decoded))
+                } catch {
+                    completion(.failure(.genericError))
+                }
+            }
+        }
+        task.resume()
     }
-    
 }
